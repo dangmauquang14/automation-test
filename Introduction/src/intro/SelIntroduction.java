14e0a4c849427a1c88cf368e5cde657ef79bf7dd
dangmauquang14 <67824338+dangmauquang14@users.noreply.github.com>
2023-09-26 11:43:11 +0700

    26.09.2023

---
+++ Introduction/src/intro/SelIntroduction.java
@@ -0,0 +1,17 @@
+package intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+	}	
+}
+
+/*
+open -a Git\ Bash
+open -a Git\ Bash
+source /usr/local/git/contrib/completion/git-completion.bash
+autoload -Uz compinit && compinit -i
+
+*/
