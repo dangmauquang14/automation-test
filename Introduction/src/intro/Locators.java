16da99e25e9007f1e4c90ec53e221ace78341026
dangmauquang14 <67824338+dangmauquang14@users.noreply.github.com>
2023-09-28 16:54:40 +0700

    28.09.2023

---
+++ Introduction/src/intro/Locators.java
@@ -0,0 +1,10 @@
+package intro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Locators {
+	public static void main(String[] args) {
+		WebDriver driver = new SafariDriver();
+	}
+}
--- Introduction/src/intro/SelIntroduction.java
+++ Introduction/src/intro/SelIntroduction.java
@@ -1,17 +1,39 @@
 package intro;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class SelIntroduction {
+
 	public static void main(String[] args) {
-		ChromeDriver driver = new ChromeDriver();
-	}	
+
+		// Invoking Browser
+		// Chrome - ChromeDriver exten->Methods close get
+		// Firefox- FirefoxDriver ->methods close get
+		// WebDriver  close  get
+		// WebDriver methods + class methods
+		
+		// Chrome
+//		ChromeOptions option=new ChromeOptions();
+//		option.setPageLoadStrategy(PageLoadStrategy.NONE);
+//		System.setProperty("webdriver.chrome.driver", "/Users/ttcenter/Study/Automation-Test/chromedriver-mac-arm64/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+
+		// Firefox
+//		System.setProperty("webdriver.gecko.driver", "/Users/ttcenter/Study/Automation-Test/geckodriver");
+//		WebDriver driver1 = new FirefoxDriver();
+
+		// Microsoft Edge
+//		System.setProperty("webdriver.edge.driver", "/Users/rahulshetty/Documents/msedgedriver");
+//		WebDriver driver2 = new EdgeDriver();
+		
+		// Safari
+		WebDriver driver = new SafariDriver();
+		
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+		//driver.quit();
+	}
 }
-
-/*
-open -a Git\ Bash
-open -a Git\ Bash
-source /usr/local/git/contrib/completion/git-completion.bash
-autoload -Uz compinit && compinit -i
-
-*/
