312df1cffb4a53a20fb6d91cdc05f55d870de74e
dangmauquang14 <67824338+dangmauquang14@users.noreply.github.com>
2025-02-12 16:59:21 +0700

    12-02-2025

--- Introduction/src/intro/Locators.java
+++ Introduction/src/intro/Locators.java
@@ -1,10 +1,22 @@
 package intro;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Locators {
+	
 	public static void main(String[] args) {
-		WebDriver driver = new SafariDriver();
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("rahu!");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		driver.close();
 	}
 }
---
+++ Introduction/src/intro/MethodsDemo2.java
@@ -0,0 +1,9 @@
+package intro;
+
+public class MethodsDemo2 {
+	public static String getUserdata() {
+		System.out.println("hello world");
+		return "rahul shetty";
+	}
+}
+
